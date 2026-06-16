dc7017d98a36eb8a6cb9aea8586e48377f668c29
Zero-i00 <mrshelkinpro228@gmail.com>
2026-06-16 11:41:17 +0500

    feat: user request middleware

--- app/bot/src/main.py
+++ app/bot/src/main.py
@@ -3,6 +3,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from core.config import get_settings
+from middlewares import RequestMiddleware
 
 settings = get_settings()
 
@@ -14,6 +15,9 @@ async def main() -> None:
     )
 
     dp = Dispatcher()
+
+    dp.message.middleware(RequestMiddleware)
+    dp.callback_query.middleware(RequestMiddleware)
 
     await bot.delete_webhook(True)
     await dp.start_polling(bot)
---
+++ app/bot/src/middlewares/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["RequestMiddleware"]
+
+from .request import RequestMiddleware
