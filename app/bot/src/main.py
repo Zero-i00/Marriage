478f4eadb9e7abf9e10b2b5f9318d57d3df40293
Zero-i00 <mrshelkinpro228@gmail.com>
2026-06-16 11:33:54 +0500

    initial commit

--- app/bot/src/main.py
+++ app/bot/src/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from bot!")
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from core.config import get_settings
+
+settings = get_settings()
+
+
+async def main() -> None:
+    bot = Bot(
+        token=settings.bot_token.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+    dp = Dispatcher()
+
+    await bot.delete_webhook(True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+
+    asyncio.run(main())
